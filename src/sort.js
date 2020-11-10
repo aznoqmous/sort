b9f40ae4f11ff55348f0ca21a31a32c0bd63ac6b
aznoqmous <paul.landreau931v@gmail.com>
2020-11-10 12:18:30 +0100

    fix order issue + styled sort span

--- src/sort.js
+++ src/sort.js
@@ -8,15 +8,46 @@ export default class Sort {
         this.currentTarget = null
 
         this.init()
+        this.buildHr()
         this.bind()
+
+        let mo = (new MutationObserver((mutations, observer)=>{
+            for(let mutation of mutations){
+                if( !mutation.addedNodes.length && !mutation.removedNodes.length ) continue;
+                if(this.elements.length == this.container.children.length - 1) continue;
+
+                console.log(this.elements, this.container.children)
+
+                this.unbind()
+                this.init()
+                this.bind()
+
+            }
+        })).observe(this.container, { childList: true })
     }
 
     init() {
-        this.elements = [...this.container.children]
+        this.elements = [...this.container.querySelectorAll(':scope > *:not(hr)')]
         this.elements.map((el, i) => {
             el.setAttribute('data-index', i)
+            if(!el.querySelector('span.sort')) {
+                let sort = document.createElement('span')
+                el.appendChild(sort)
+                sort.className = 'sort'
+                sort.innerHTML = '&#11205;&#11206;'
+                let styles = {
+                    'font-size': '0.8rem',
+                    'letter-spacing': '-0.8em',
+                    transform: 'translate(0, -0.3em)',
+                    'writing-mode': 'vertical-lr',
+                    cursor: 'n-resize',
+                }
+                for(let key in styles) sort.style[key] = styles[key]
+            }
         })
+    }
 
+    buildHr(){
         this.hr = document.createElement('hr')
         let styles = {
             opacity: 0,
@@ -27,8 +58,22 @@ export default class Sort {
             margin: 0
         }
         for(let key in styles) this.hr.style[key] = styles[key]
+        this.container.appendChild(this.hr)
     }
 
+    unbind(){
+        let mousedown = (e) => {
+            this.currentTarget = e.currentTarget.parentElement
+            this.initialPos = e.currentTarget.parentElement.getBoundingClientRect()
+            this.initialCursorPos = {x: e.pageX, y: e.pageY}
+
+            this.container.addEventListener('mousemove', move)
+            document.body.addEventListener('mouseup', mouseup)
+        }
+        this.elements.map(el => {
+            el.querySelector('span.sort').removeEventListener('mousedown', mousedown)
+        })
+    }
     bind() {
         let move = (e)=>{
             this.move(e, this.currentTarget)
@@ -41,17 +86,18 @@ export default class Sort {
             this.applyMove()
         }
 
+        let mousedown = (e) => {
+            this.currentTarget = e.currentTarget.parentElement
+            this.initialPos = e.currentTarget.parentElement.getBoundingClientRect()
+            this.initialCursorPos = {x: e.pageX, y: e.pageY}
+
+            this.container.addEventListener('mousemove', move)
+            document.body.addEventListener('mouseup', mouseup)
+        }
+
         this.elements.map(el => {
-            el.addEventListener('mousedown', (e) => {
-                this.currentTarget = el
-                this.initialPos = e.currentTarget.getBoundingClientRect()
-                this.initialCursorPos = {x: e.pageX, y: e.pageY}
-
-                this.container.addEventListener('mousemove', move)
-                document.body.addEventListener('mouseup', mouseup)
-            })
+            el.querySelector('span.sort').addEventListener('mousedown', mousedown)
         })
-
     }
 
     move(e, el) {
@@ -61,7 +107,7 @@ export default class Sort {
         el.transformedX = cursorDiff.x
         el.transformedY = cursorDiff.y
         el.style.zIndex = 10000
-        el.style.opacity = 0.6
+        el.style.opacity = 0.8
 
         el.setAttribute('data-active', true)
         let hrPosition = this.getHrPosition()
@@ -81,7 +127,6 @@ export default class Sort {
 
     applyMove() {
         this.elements = this.getSortedElements()
-        this.container.innerHTML = ''
         this.elements.map((el, i) => {
             el.setAttribute('data-index', i)
             el.style.transform = ''
@@ -89,27 +134,28 @@ export default class Sort {
             el.removeAttribute('data-active')
             this.container.appendChild(el)
         })
+        this.hr.style.opacity = 0
     }
 
     getSortedElements() {
         return this.elements
             .map((el)=>{return el})
             .sort((a, b) => {
-                let ay = a.getBoundingClientRect().y
-                if(a.transformedY) ay += a.transformedY
-                let by = b.getBoundingClientRect().y
-                if(b.transformedY) by += b.transformedY
+                let ay = a.getBoundingClientRect().y + a.getBoundingClientRect().height / 2
+                let by = b.getBoundingClientRect().y + b.getBoundingClientRect().height / 2
                 return (ay > by) ? 1 : -1;
-        })
+            })
     }
 
     getHrPosition(){
         let currentEl = this.elements.filter(el => el.getAttribute('data-active'))[0].getAttribute('data-index')
 
-        this.sortedElements = this.getSortedElements()
-        let sortedIndices = this.sortedElements.map(el => el.getAttribute('data-index'))
+        let sortedElements = this.getSortedElements()
+        let sortedIndices = sortedElements.map(el => el.getAttribute('data-index'))
         let indices = this.elements.map(el => el.getAttribute('data-index'))
         if(sortedIndices.join() === indices.join()) return null;
+
+        console.log(sortedElements, this.elements)
 
         for(let index of sortedIndices){
             if(sortedIndices[index] === currentEl) return parseInt(index);
